4c7f9a7a53d79bc022189dcab1b9ec8f77e69d70
mario emad <mario emad@LAPTOP-7A47C1CF>
2024-11-07 20:58:14 +0200

    Add project files.

---
+++ Company.Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Company.Repository/Repositories/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using Company.Data.Models;
+using Company.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Repository.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
