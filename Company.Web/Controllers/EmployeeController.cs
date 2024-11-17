88cc9e16a1c9ad6491c53cc3d068557628afba16
mario emad <mario emad@LAPTOP-7A47C1CF>
2024-11-17 23:32:03 +0200

    Employee Create & ViewBag - ViewData - TempData

--- Company.Web/Controllers/EmployeeController.cs
+++ Company.Web/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Company.Service.Interfaces;
+﻿using Company.Data.Models;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Web.Controllers
@@ -6,10 +8,12 @@ namespace Company.Web.Controllers
     public class EmployeeController : Controller
     {
         private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
 
-        public EmployeeController(IEmployeeService employeeService)
+        public EmployeeController(IEmployeeService employeeService, IDepartmentService departmentService)
         {
             _employeeService = employeeService;
+            _departmentService = departmentService;
         }
         [HttpGet]
         public IActionResult Index(string searchInp)
@@ -17,6 +21,9 @@ namespace Company.Web.Controllers
             if(string.IsNullOrEmpty(searchInp))
             {
                 var emp = _employeeService.GetAll();
+                //ViewBag.Message = "This is Message From ViewBag";
+                //ViewData["TxtMessage"] = "This is Message From ViewData";
+                //TempData["TxtMessage2"] = "This is Message From TempData";
                 return View(emp);
             }
             else
@@ -25,6 +32,34 @@ namespace Company.Web.Controllers
                 return View(emp);
             }
             
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Departments = _departmentService.GetAll();  
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeService.Add(employee);
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("EmployeeError", "ValidationErrors");
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("EmployeeError", ex.Message);
+                return View(employee);
+            }
         }
     }
 }
